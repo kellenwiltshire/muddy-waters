e0e9b6554557112c479f92323484a26ee462f166
Kellen Wiltshire <kellenwiltshire@outlook.com>
2021-06-02 16:00:15 -0400

    Adding motion

--- pages/index.js
+++ pages/index.js
@@ -2,11 +2,18 @@ import Hero from '../components/Home/Hero';
 import Location from '../components/Layout/Location';
 import Menu from '../components/Layout/Menu';
 import Titles from '../components/Layout/Titles';
+import { motion } from 'framer-motion';
 
 export default function Home() {
 	return (
 		<div>
-			<Hero />
+			<motion.div
+				initial={{ opacity: 0 }}
+				animate={{ opacity: 1 }}
+				transition={{ duration: 1 }}
+			>
+				<Hero />
+			</motion.div>
 			<Titles id={'menu'} title='Menu' />
 			<Menu />
 			<Titles id={'location'} title='Location' />
